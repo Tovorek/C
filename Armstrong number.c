6b800f2e738393ced7bb9dcbe8f951e45e66976c
Arrow <71452112+buhuhaha@users.noreply.github.com>
2022-02-22 13:05:10 +0530

    Q2

---
+++ Armstrong number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	int number,i=0,n,result=0,number1,temp;
+	printf("enter the number:");
+	scanf("%d",&number);
+	number1=number;
+	temp=number;
+	while(number!=0){
+		number=number/10;
+		i++;
+	}
+
+	while(number1!=0){
+		n=number1%10;
+		result=result+pow(n,i);
+		number1=number1/10;
+		}
+
+	if(temp==result)
+		printf("number is armstrong");
+		else
+		printf("not a armstrong");
+	return 0;
+
+} 
